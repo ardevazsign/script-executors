8e70eb3f39417ca810482f52433a841e0f966bfc
Freaklord11 <141641366+Freaklord11@users.noreply.github.com>
2024-04-06 13:28:11 +0800

    Inital Page Genertation

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 
-const API_KEY = 'api_key=a51a9bb0cd5bc1ed985d30c82a20bd57';
-const BASE_URL = 'https://api.themoviedb.org/3';
+/**const API_KEY = 'api_key=a51a9bb0cd5bc1ed985d30c82a20bd57';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
 const API_URL = BASE_URL + '/dicover/movie?sort_by=papularity.desc&'
     + API_KEY;
@@ -48,6 +48,69 @@ form.addEventListener('submit', (e) => {
     if (searchTerm) {
         getMovies(searchURL+'&query='+searchTerm) 
     }
+})*/
+import axios from "axios";
+import { Notify } from "notiflix/build/notiflix-notify-aio.js";
+import {BASE_URL,trending_path, trend_options, imageUrl, API_KEY} from "./filmoteka-api.js";
+
+window.addEventListener('load', loadMovieList);
+const movieEl = document.querySelector('.movie-list');
+
+async function loadMovieList() {
+    try {
+        // Fetch data
+        const path = BASE_URL + trending_path;
+        const res = await axios.get(path, trend_options);
+        console.log(res.data);
 
 
-})
+        // Destructure movieList from the response data
+        const { results } = res.data;
+        // Call createMovie function with movieList as argument
+        createMovie(results);
+    } catch (e) {
+        console.error('Error loading movie list:', e);
+        Notify.failure(e);
+    }
+}
+
+//create movie list
+
+async function createMovie(results){
+    // Fetch genre information
+    const genreResponse = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+    const genreData = await genreResponse.json();
+    const genres = genreData.genres;
+
+    let movieMarkup = 
+    results.map((
+        {
+            id,
+            title,
+            original_title,
+            backdrop_path,
+            poster_path,
+            genre_ids,
+            release_date,
+        }
+    )=> {
+        // Map genre_ids to genre names
+        const movieGenres = genre_ids.slice(0, 2).map(genreId => {
+            const genre = genres.find(genre => genre.id === genreId);
+            return genre ? genre.name : 'Unknown';
+        }).join(', ');
+        //Movie year
+        const year = new Date(release_date).getFullYear();
+
+        return `
+        <div class ="movie-card">
+        <image src="${imageUrl+poster_path}" alt ="${original_title}"/>
+        <div class ="movieCardDesc">
+            <h2 class="movie-title">${title}<h2/>
+            <span class ="movie-preview">${movieGenres}|${year}</span>
+        </div>
+        </div>
+        `;
+    }).join("");
+movieEl.insertAdjacentHTML('beforeend', movieMarkup);
+}
